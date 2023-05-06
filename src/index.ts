f9efb0976219e3724738d03f7986f77e8b071d44
anaomart <anaomart@gmail.com>
2023-05-06 21:32:49 +0300

    simple todo list with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,56 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidv4} from 'uuid'
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.querySelector('#newTaskForm') as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>('#newTask')
+type taskType = {
+  id:string ,
+  title:string,
+  completed:boolean,
+  createdAt:Date
+}
+const tasks : taskType[] = loadTasks();
+tasks.forEach(addListItem)
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-import confetti from 'canvas-confetti';
+  if (input?.value == '' || input?.value == null) return;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+  const task : taskType= {
+    id :uuidv4(),
+    title:input.value,
+    completed:false,
+    createdAt:new Date()
+  }
+  tasks.push(task)
+
+  addListItem(task)
+  input.value =''
+})
+
+function addListItem(task : taskType) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox'
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked
+    console.log(tasks)
+  })
+
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item);
+  saveTasks();
+}
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks))
+}
+function loadTasks():taskType[] 
+ {
+  const taskJSON = localStorage.getItem('TASKS')
+  if (taskJSON == null) return [];
+  const tasks :taskType[]   = JSON.parse(taskJSON)
+  return tasks;
+}
+console.log(list)
